4425b06d5af55a71c9862403001d4364a0056108
= <jnewall93@outlook.com>
2022-10-02 14:48:50 +0100

    sym linked

---
+++ nvim/init.lua
@@ -0,0 +1,25 @@
+vim.g.mapleader = ','
+vim.opt.number = true
+vim.opt.mouse = 'a'
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = false
+vim.opt.wrap = true
+vim.opt.breakindent = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.relativenumber=true
+    
+
+
+
+ 
+
+
+
+vim.keymap.set({'n','i','v'},'<C-C>','+y',{noremap=true, nowait=true})
+vim.keymap.set('n', '<C-V>', '+p',{noremap=true, nowait=true})
+
+
+	    		
