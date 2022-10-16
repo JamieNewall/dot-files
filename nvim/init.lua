794aeb12c757254ed5e8a39b93b7829b4f9d16a7
= <jnewall93@outlook.com>
2022-10-16 10:00:01 +0100

    automated commit

--- nvim/init.lua
+++ nvim/init.lua
@@ -168,6 +168,88 @@ end
             end
     }
 
+
+
+--setup null ls and prettier
+local null_ls = require("null-ls")
+
+null_ls.setup({
+  on_attach = function(client, bufnr)
+    if client.server_capabilities.documentFormattingProvider then
+        print('yes this is available')
+        vim.cmd("nnoremap <silent><buffer> <Leader>f :lua vim.lsp.buf.formatting()<CR>")
+
+
+      -- format on save
+      vim.cmd("autocmd BufWritePost <buffer> lua vim.lsp.buf.formatting()")
+    end
+
+    if client.server_capabilities.documentRangeFormattingProvider then
+      vim.cmd("xnoremap <silent><buffer> <Leader>f :lua vim.lsp.buf.range_formatting({})<CR>")
+    end
+  end,
+})
+
+
+local prettier = require("prettier")
+
+prettier.setup({
+  bin = 'prettier', -- or `'prettierd'` (v0.22+)
+  filetypes = {
+    "css",
+    "graphql",
+    "html",
+    "javascript",
+    "javascriptreact",
+    "json",
+    "less",
+    "markdown",
+    "scss",
+    "typescript",
+    "typescriptreact",
+    "yaml",
+  },
+   ["null-ls"] = {
+    condition = function()
+      return prettier.config_exists({
+        -- if `true`, checks `package.json` for `"prettier"` key
+        check_package_json = false,
+      })
+    end,
+    runtime_condition = function(params)
+      -- return false to skip running prettier
+      return true
+    end,
+    timeout = 5000,
+  },
+   cli_options = {
+    arrow_parens = "always",
+    bracket_spacing = true,
+    bracket_same_line = false,
+    embedded_language_formatting = "auto",
+    end_of_line = "lf",
+    html_whitespace_sensitivity = "css",
+    -- jsx_bracket_same_line = false,
+    jsx_single_quote = false,
+    print_width = 80,
+    prose_wrap = "preserve",
+    quote_props = "as-needed",
+    semi = true,
+    single_attribute_per_line = false,
+    single_quote = false,
+    tab_width = 2,
+    trailing_comma = "es5",
+    use_tabs = false,
+    vue_indent_script_and_style = false,
+  },
+   cli_options = {
+    -- https://prettier.io/docs/en/cli.html#--config-precedence
+    config_precedence = "prefer-file", -- or "cli-override" or "file-override"
+  },
+})
+
+
+
 vim.opt.completeopt={"menu","menuone","noselect"}
 
   -- Set up nvim-cmp and snippets
